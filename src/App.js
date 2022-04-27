5154da7f11beaa1c6c287634ea3291311f507474
Emily Miller <em913278@wcupa.edu>
2022-04-27 15:44:24 -0400

    nodemodules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/aboutus';
+import Wine from './pages/wine';
+import Sales from './pages/sales';
+import Beer from './pages/beer';
+import Spirits from './pages/spirits';
+import Home from './pages/home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+		<Route path='/wine' component={Wine} />
+		<Route path='/sales' component={Sales} />
+		<Route path='/beer' component={Beer} />
+    <Route path='/spirits' component={Spirits} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
