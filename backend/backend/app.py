b211acf4ff6df5c811b70d560f1bf9e4dce4de35
Cade Uhlin <ctuhlin@gmail.com>
2022-04-27 21:54:06 -0400

    backend

---
+++ backend/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('./moola-miners/webui/public/index.html')
+
+
+@app.route('/index.html')
+def my_home2():
+    return render_template('./moola-miners/webui/public/index.html')
