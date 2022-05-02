0286df183218aabf930f2b4f2a45bcc8848119c5
Cade Uhlin <66435114+CadeUhlin@users.noreply.github.com>
2022-05-02 16:44:57 -0400

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,1 +1,21 @@
 from flask import Flask, Response
+import os
+import socket
+import time
+
+app = Flask(__name__)
+
+# Enable debugging if the DEBUG environment variable is set and starts with Y
+app.debug = os.environ.get("DEBUG", "").lower().startswith('y')
+
+hostname = socket.gethostname()
+
+urandom = os.open("/dev/urandom", os.O_RDONLY)
+
+
+@app.route("/")
+def index():
+    return "APP running on {}\n".format(hostname)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, threaded=False)
