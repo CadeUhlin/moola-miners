c72a620a10c65d803ea07a621c7a110186d5da8e
Cade Uhlin <66435114+CadeUhlin@users.noreply.github.com>
2022-05-02 16:42:35 -0400

    Update app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, url_for, request, redirect
-import csv
+from flask import Flask, Response
+import os
+import socket
+import time
 
 app = Flask(__name__)
-print(__name__)
+
+# Enable debugging if the DEBUG environment variable is set and starts with Y
+app.debug = os.environ.get("DEBUG", "").lower().startswith('y')
+
+hostname = socket.gethostname()
+
+urandom = os.open("/dev/urandom", os.O_RDONLY)
 
 
-@app.route('/')
-def my_home():
-    return render_template('./moola-miners/webui/public/index.html')
+@app.route("/")
+def index():
+    return "APP running on {}\n".format(hostname)
 
-
-@app.route('/index.html')
-def my_home2():
-    return render_template('./moola-miners/webui/public/index.html')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, threaded=False)
