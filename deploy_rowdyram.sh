bbf6f22e683f5eb80b2e6bea5e1742780c27352d
Cade Uhlin <66435114+CadeUhlin@users.noreply.github.com>
2022-05-02 17:05:26 -0400

    Update deploy_rowdyram.sh

--- deploy_rowdyram.sh
+++ deploy_rowdyram.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -x
-namespaceStatus=$(kubectl get namespaces ramcoin -o json | jq .status.phase -r)
+namespaceStatus=$(kubectl get namespaces rowdyrams -o json | jq .status.phase -r)
 
 if [ $namespaceStatus == "Active" ]
 then
-    echo "Namespace ramcoin exists, need to clean up"
-    kubectl delete namespaces ramcoin
+    echo "Namespace rowdyrams exists, need to clean up"
+    kubectl delete namespaces rowdyrams
 fi
 
-echo "Creating namespace ramcoin"
-kubectl create namespace ramcoin 
+echo "Creating namespace rowdyrams"
+kubectl create namespace rowdyrams
  
 echo "Creating pods"
-kubectl create -f ramcoin.yaml --namespace ramcoin
+kubectl create -f rowdyrams.yaml --namespace rowdyrams
 
 echo "Creating services"
-kubectl create -f ramcoin-service.yaml --namespace ramcoin
+kubectl create -f rowdyrams-service.yaml --namespace rowdyrams
 
 
-kubectl get pods -n ramcoin
+kubectl get pods -n rowdyrams
 
